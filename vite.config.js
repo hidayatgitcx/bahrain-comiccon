3b7b1ac3c7bce1ac7a2f2f332551f0db8c891e3f
hidayatgitcx <108321436+hidayatgitcx@users.noreply.github.com>
2025-12-02 13:53:01 +0300

    fixed vite path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: './',
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/bahrain-comiccon"
+  base: process.env.VITE_BASE_PATH || "/bahrain-comiccon",
 });
